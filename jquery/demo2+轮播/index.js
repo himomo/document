5f07fad32f061b41fb7eb3d44276859c2bb84f92
himomo <1584690229@qq.com>
2019-08-30 14:37:20 +0800

    Revert "Jquery"

--- jquery/demo2+轮播/index.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $("#silder_banner").silderBanner({
-        imgs: ['../demo/images/1.jpg', '../demo/images/2.jpg', '../demo/images/3.jpg', '../demo/images/4.jpg', '../demo/images/5.jpg']
-    });
-})
